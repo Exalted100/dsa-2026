f481dcf0aa5d531ae7a9182bb22904810337b65a
Exalted100 <harounayomikun@gmail.com>
2025-08-07 15:01:52 +0100

    contains duplicates

---
+++ blind-75/arrays/containsDuplicates.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/contains-duplicate/description/?envType=problem-list-v2&envId=oizxjoit
+
+var containsDuplicate = function(nums) {
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]]) {
+            return true;
+        }
+
+        map[nums[i]] = true;
+    }
+
+    return false;
+};
+
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));

3814be5dbdbb0af3b824cd36d01357dff6e3bd8d
Exalted100 <harounayomikun@gmail.com>
2025-08-07 13:32:17 +0100

    two sum solution

--- blind-75/twoSum.js
+++ blind-75/twoSum.js
@@ -1,10 +1,29 @@
 // https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=oizxjoit
 
 var twoSum = function(nums, target) {
-    const sortedNums = nums.sort();
+    const sortedNums = [...nums].sort((a, b) => a - b);
+    let first, second;
 
-    for (let left = 0, right = 1; right < sortedNums.length; i++) {}
+    for (let left = 0, right = sortedNums.length - 1; right > left; null) {
+        if (sortedNums[left] + sortedNums[right] > target) {
+            right--;
+            continue;
+        } else if (sortedNums[left] + sortedNums[right] < target) {
+            left++;
+            continue;
+        } else {
+            first = sortedNums[left];
+            second = sortedNums[right];
+            break;
+        }
+    }
+
+    const firstIndex = nums.indexOf(first);
+    const secondIndex = nums.lastIndexOf(second);
+
+    return [firstIndex, secondIndex];
 };
 
 console.log(twoSum([2,7,11,15], 9));
 console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));

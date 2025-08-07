c0b06764210b10f75a6f7a71e2535582c33a2a2f
Exalted100 <harounayomikun@gmail.com>
2025-08-07 13:15:45 +0100

    first commit

---
+++ blind-75/twoSum.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=oizxjoit
+
+var twoSum = function(nums, target) {
+    const sortedNums = nums.sort();
+
+    for (let left = 0, right = 1; right < sortedNums.length; i++) {}
+};
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
